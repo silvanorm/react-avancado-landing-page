c471fbeeacb126ac2ab0bb5e55d8961447eaa938
Silvano RM <15691851+silvanorm@users.noreply.github.com>
2020-10-09 20:57:11 -0300

    starting to add the first graphql queries

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,5 +1,9 @@
 import { GraphQLClient } from 'graphql-request';
 
-const client = new GraphQLClient('http://localhost:1337/graphql');
+const client = new GraphQLClient('http://localhost:1337/graphql', {
+  headers: {
+    Authorization: `Bearer ${process.env.MY_TOKEN}`
+  }
+});
 
 export default client;
